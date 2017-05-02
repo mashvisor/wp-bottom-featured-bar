6c2014c4cf2da21989708a4e37a086fe1fb02dd5
Yousef Omar <yousef@mashvisor.com>
2017-05-02 12:55:42 +0300

    adding custom hidden-xs-mv class, that hides the element on mobile devices

--- public/partials/bottom-featured-bar-public-display.php
+++ public/partials/bottom-featured-bar-public-display.php
@@ -15,7 +15,7 @@
 $_post = wp_bottom_featured_bar_post();
 if($_post != null):  ?>
 
-<div class="bottom-featured-bar hidden-xs" id="bottom-featured-bar">
+<div class="bottom-featured-bar hidden-xs-mv" id="bottom-featured-bar">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
