29d85f042013c3bcbbb19d275982dcb80306ea60
Mohammed Jebrini <mohammed.jebrini@gmail.com>
2017-05-02 11:17:01 +0300

    feat: include category parameter in filtering featured posts

--- includes/class-bottom-featured-bar-template-tags.php
+++ includes/class-bottom-featured-bar-template-tags.php
@@ -13,12 +13,22 @@
 function wp_bottom_featured_bar_post() {
     global $post;
     // show only if the post type is a blog post
-    if($post->post_type != "post")
+    if($post && $post->post_type != "post")
         return null;
 
-    $args = array( 'posts_per_page' => 1, 'orderby' => 'rand' ); 
+    // load categories 
+    $categories = wp_get_post_categories($post->ID);
+    $args = array( 'posts_per_page' => 5, 'orderby' => 'rand', 'exclude' => $post->ID ); 
+
+    if ( $count = count($categories) > 0 ) {
+       if ( $count == 1 && $categories[0] == 1) {
+            // ignore the filter.
+        } else {
+            $args['category'] = implode($categories, ",");
+        }
+    }
+
     $rand_posts = get_posts( $args );
 
     return $rand_posts[0];
-    
 }
